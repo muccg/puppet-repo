d47d080b0cc867dcce9ad0b5e554786b0f1d63fb
Adam Hunter <ahunter@ccg.murdoch.edu.au>
2014-11-24 16:27:21 +0800

    ccgdeps repo

---
+++ manifests/repo/ccgdeps.pp
@@ -0,0 +1,22 @@
+#
+class repo::repo::ccgdeps($priority='70') {
+  include stdlib
+
+  class { 'repo::repo::ccgdeps::setup':
+    priority => $priority,
+    stage    => 'setup',
+  }
+}
+
+
+class repo::repo::ccgdeps::setup($priority='70') {
+
+  yum::managed_yumrepo { 'ccg-deps':
+    descr          => 'CCG Packages for Enterprise Linux 6 - Deps - $basearch',
+    baseurl        => 'http://repo.ccgapps.com.au/repo/ccg-deps/centos/6/os/$basearch',
+    enabled        => 1,
+    gpgcheck       => 0,
+    priority       => $priority,
+  }
+
+}
