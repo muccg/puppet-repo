843efa2d6a67dcacabd2ad4552d63c30ddcc9b0f
Adam Hunter <ahunter@ccg.murdoch.edu.au>
2014-11-24 11:35:38 +0800

    use yum::managed_yumrepo to define IUS

--- manifests/repo/ius.pp
+++ manifests/repo/ius.pp
@@ -11,22 +11,19 @@ class repo::repo::ius($arch='x86_64', $priority='20') {
 
 
 class repo::repo::ius::setup($arch='x86_64', $priority='20') {
-  # there is an ius-release RPM available but it has a dependency on
-  # the epel-release package being installed. Since we use the
-  # stahnma-epel puppet module, this dependency would never be
-  # satisfied. So instead, the repo is setup by hand.
-  yumrepo { 'ius':
-    descr      => "IUS Community Packages for Enterprise Linux 6 - ${arch}",
-    mirrorlist => "http://dmirr.iuscommunity.org/mirrorlist/?repo=ius-centos${::majdistrelease}&arch=${arch}",
-    enabled    => 1,
-    gpgcheck   => 0
+  yum::managed_yumrepo { 'ius':
+    descr          => "IUS Community Packages for Enterprise Linux 6 - ${arch}",
+    mirrorlist     => "http://dmirr.iuscommunity.org/mirrorlist/?repo=ius-centos${::majdistrelease}&arch=${arch}",
+    enabled        => 1,
+    gpgcheck       => 0,
+    priority       => $priority,
   }
 
-  yumrepo { 'ius-source':
-    descr      => "IUS Community Packages for Enterprise Linux 6 - ${basearch} - Source",
-    #mirrorlist => "http://dmirr.iuscommunity.org/mirrorlist/?repo=ius-centos${::majdistrelease}&arch=${arch}",
-    baseurl    => "http://dl.iuscommunity.org/pub/ius/stable/CentOS/6/SRPMS",
-    enabled    => 0,
-    gpgcheck   => 1
+  yum::managed_yumrepo { 'ius-source':
+    descr          => "IUS Community Packages for Enterprise Linux 6 - ${arch} - Source",
+    mirrorlist     => "http://dmirr.iuscommunity.org/mirrorlist/?repo=ius-centos${::majdistrelease}&arch=source",
+    enabled        => 0,
+    gpgcheck       => 0,
   }
+
 }
