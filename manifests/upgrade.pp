e31851290fd71cdeb33a779dc2a7efb8bd36b884
Adam Hunter <ahunter@ccg.murdoch.edu.au>
2014-11-25 12:26:12 +0800

    Ensure update happens before upgrade.

--- manifests/upgrade.pp
+++ manifests/upgrade.pp
@@ -29,14 +29,18 @@ class repo::upgrade::yum-update (
     command => "/usr/bin/yum -y -q ${yum_options} update",
     timeout => 0,
   }
+
+  Class['repo::update::yum-check-update'] -> Class['repo::upgrade::yum-update']
 }
 
 
-class repo::upgrade::apt-upgrade(
+class repo::upgrade::apt-upgrade (
   $apt_options = '',
   ) {
   exec { 'apt upgrade':
     command => "/usr/bin/apt-get -y -q ${apt_options} upgrade",
     timeout => 0,
   }
+
+  Class['apt::update'] -> Class['repo::upgrade::apt-upgrade']
 }
